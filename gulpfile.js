6f42c130390c0efc2e64a35c5eb2036076dbc4ff
sonic <sonic0828@gmail.com>
2017-05-21 08:33:02 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/**
+ * iamsonic.net Workflow Automation
+ * 
+ * Author: Sonic
+ * LastModify: 2017-05-21 
+ */
+
+var gulp 		  = require('gulp');
+
+/*
+ * Setting Pathsstella
+ */
+var paths = {
+  web  :  "public/"
+};
+
+/* ---------------------------------------------------
+ * Task website Build [向/ 目录输出public网页文件]
+ */
+gulp.task('build-blog', function(){
+  return gulp.src([paths.web+'**/*'])
+    .pipe(gulp.dest('/'))
+})
